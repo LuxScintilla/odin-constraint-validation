50fba810bd25ff70390b44d87de6c223bc372a0f
LuxScintilla <105556122+LuxScintilla@users.noreply.github.com>
2024-03-02 16:42:08 +0000

    add username email validation

---
+++ main.js
@@ -0,0 +1,106 @@
+const app = {
+  init() {
+    app.addListeners();
+  },
+
+  addListeners() {
+    const form = document.querySelector(".form");
+    const userNameInput = document.getElementById("username");
+    const emailInput = document.getElementById("email");
+    const countryInput = document.getElementById("country");
+    const postcodeInput = document.getElementById("postcode");
+    const passwordInput = document.getElementById("password");
+    const passwordConfirm = document.getElementById("password-confirm");
+
+    userNameInput.addEventListener("input", app.testUserName);
+    userNameInput.addEventListener("keypress", app.disableSpaceKey);
+
+    emailInput.addEventListener("input", app.testEmail);
+    emailInput.addEventListener("keypress", app.disableSpaceKey);
+  },
+
+  testUserName(e) {
+    const username = e.target;
+    username.setCustomValidity("");
+
+    if (app.containsSpecialCharacters(username.value) === true) {
+      username.setCustomValidity(
+        "Username my not contain any special characters"
+      );
+      username.reportValidity();
+    } else if (username.value.length < 3 || username.value.length > 20) {
+      username.setCustomValidity(
+        "Username must be between 3 and 20 characters"
+      );
+      app.errorMessage(e, "Username must be between 3 and 20 characters");
+    } else if (username.value.length >= 3 && username.value.length <= 20) {
+      app.errorMessage(e, "");
+    }
+
+    if (username.checkValidity()) {
+      e.target.parentElement
+        .querySelector(".fa-circle-check")
+        .classList.remove("hidden");
+    } else {
+      e.target.parentElement
+        .querySelector(".fa-circle-check")
+        .classList.add("hidden");
+    }
+  },
+
+  testEmail(e) {
+    const email = e.target;
+    email.setCustomValidity("");
+
+    const emailRegex = new RegExp(
+      /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+    );
+    if (emailRegex.test(email.value) === false) {
+      email.setCustomValidity("Enter a valid email address");
+      app.errorMessage(e, "Enter a valid email address");
+    } else if (emailRegex.test(email.value) === true) {
+      email.setCustomValidity("");
+      app.errorMessage(e, "");
+    }
+
+    if (email.checkValidity()) {
+      e.target.parentElement
+        .querySelector(".fa-circle-check")
+        .classList.remove("hidden");
+    } else {
+      e.target.parentElement
+        .querySelector(".fa-circle-check")
+        .classList.add("hidden");
+    }
+  },
+
+  containsSpecialCharacters(str) {
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    return specialChars.test(str);
+  },
+
+  disableSpaceKey(e) {
+    const key = e.keyCode;
+    if (key === 32) {
+      e.preventDefault();
+    }
+  },
+
+  errorMessage(e, message) {
+    const field = e.target;
+    const errorField = field.parentElement.querySelector(".error-span");
+
+    switch (field.id) {
+      case "username":
+        errorField.textContent = message;
+        break;
+      case "email":
+        errorField.textContent = message;
+        break;
+      default:
+        console.log("something went wrong: default switch");
+    }
+  },
+};
+
+document.addEventListener("DOMContentLoaded", app.init);
